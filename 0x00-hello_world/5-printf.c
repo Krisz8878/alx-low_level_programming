811a482c5ef1d9552720e7205033ac5d9a72daa3
Krisz8878 <christianutsu@gmail.com>
2022-09-08 09:13:35 -0700

    right push

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,13 +1,14 @@
 #include <stdio.h>
 
 /**
- * main - Print a string
+ *  * main - Entry point
  * Description: use printf
  * Return: 0
  */
 
 int main(void)
+
 {
-	printf("with proper grammer, but the outcome is a piece of art,\n");
-	return(0)
+	printf("%s", "with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
 }
