8cc1029207715b21856ec2df047a30851b88df0c
Krisz8878 <christianutsu@gmail.com>
2022-09-09 09:53:36 -0700

    correct coding style

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -10,9 +10,10 @@ int main(void)
 	int i;
 
 	for (i ='0'; i <= '9'; i++)
+	
 	{
 		putchar(i);
-
+		
 		if (i != '9')
 		{
 			putchar(',');
@@ -21,5 +22,5 @@ int main(void)
 	}
 	
 	putchar('\n');
-return (0);
+	return (0);
 }
