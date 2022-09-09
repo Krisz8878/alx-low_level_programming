5af1c26049389f595a2370a4fc23e98ad25b1951
Krisz8878 <christianutsu@gmail.com>
2022-09-09 12:14:51 -0700

    have tried

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,32 +1,29 @@
 #include <stdio.h>
-#include <unistd.h>
 /**
- * main - Entry point
- * Description: prints two digits combination
+ * main - entry point
+ * Description: print combinations of three numbers
  * Return: Always 0 (success)
  */
 int main(void)
 {
-	int c, i;
+	int digit1, digit2;
 
-	for (c = '0'; c <= '9'; c++)
+	for (digit1 = 0; digit1 < 9; digit1++)
 	{
+		for (digit2 = digit1 + 1; digit2 < 10; digit2++)
+		{
+			putchar((digit1 % 10) + '0');
+			putchar((digit2 % 10) + '0');
 
-		for (i = '0'; i <= '9'; i++)
-			if (c < i)
-			{
-				putchar(c);
-				putchar(i);
+			if (digit1 == 8 && digit2 == 9)
+				continue;
 
-				if (c != '8' || (c == '8' && i != '9'))
-				{
-					putchar(',');
-					putchar(' ');
-				}
-			}
+			putchar(',');
+			putchar(' ');
+		}
 	}
-}
-putchar('\n');
-return (0);
-}
 
+	putchar('\n');
+
+	return (0);
+}
