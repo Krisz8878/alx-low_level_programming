283752802140d6bf12b35b2f744442c629f78aa2
Krisz8878 <christianutsu@gmail.com>
2022-09-13 02:55:11 -0700

    prints putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/**
+ * main - the entry point
+ * Description: prints _putchar
+ * Return: Always 0 on success
+ */
+int _putchar(char c)
+{
+	return (write (1, &c, 1));
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+This directory contains solutions of the tasks given by alx software engineering program done by Utsu, Christian Akomaye of cohort 9.
