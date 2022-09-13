64835e003647729b8112c797a1b72c766b4851f1
Krisz8878 <christianutsu@gmail.com>
2022-09-13 12:04:35 -0700

    finally

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -3,9 +3,20 @@
 /**
  * main - the entry point
  * Description: prints _putchar
- * Return: Always 0 on success
+ * Return: nothing, return void
  */
-char _putchar(char c)
+
+int main(void)
 {
-	return (write (1, &c, 1));
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+
+	return (0);
 }
