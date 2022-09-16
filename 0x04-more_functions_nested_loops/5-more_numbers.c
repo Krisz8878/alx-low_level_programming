0664163963b8227e2475ddd5550bdb9baa2f5655
Krisz8878 <christianutsu@gmail.com>
2022-09-16 06:18:16 -0700

    fixed

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -15,9 +15,9 @@ void more_numbers(void)
 	{
 		for (y = 0; y <= 14; y++)
 		{
-			if (y > 9)
-				_putchar(y / 10 + '0');
-			_putchar(y % 10 + '0');
+			if (y >= 10)
+				_putchar((y / 10) + '0');
+			_putchar((y % 10) + '0');
 		}
 		_putchar('\n');
 	}
