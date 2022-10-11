3ca06ee40890fc2a9d23db11e47cd9bce8cb378f
Krisz8878 <christianutsu@gmail.com>
2022-10-11 18:26:21 +0000

    task 3 and four

---
+++ 0x0D-preprocessor/4-sum.h
@@ -0,0 +1,12 @@
+#ifndef SUM_H
+#define SUM_H
+
+/**
+ * File: 4-sum.h
+ * Desc: header file that defines a function-like macro SUM(x, y)
+ * that computes the sum of two numbers.
+ */
+
+#define SUM(x, y) ((x) + (y))
+
+#endif /* SUM_H */
